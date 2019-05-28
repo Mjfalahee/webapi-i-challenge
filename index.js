1b85b8249db92dc7182a77d81a5258ee39aa7ce5
mjfalahee <mjfalahee@gmail.com>
2019-05-28 12:52:14 -0700

    Done with MVP

--- index.js
+++ index.js
@@ -46,8 +46,114 @@ server.post('/api/users', (req, res) => {
                 message: 'There was an error while saving the user to the database',
                 err,
             })
+        });
+});
+
+
+server.get('/api/users/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(user => {
+            if (user) {
+                res.status(200).json({
+                    success: true,
+                    user,
+                });
+            }
+            else {
+                res.status(404).json({
+                    success: false,
+                    message: 'The user with the specified ID does not exist.',
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                message: 'The user information could not be retrieved.'
+            });
+        });
+    });
+
+
+server.delete('/api/users/:id', (req, res) => {
+    
+    db.remove(req.params.id)
+        .then(user => {
+            if (user) {
+                res.status(200).json({
+                    success: true,
+                    message: 'User removed',
+                })
+            }
+            else {
+                res.status(404).json({
+                    success: false,
+                    message: 'The user with the specified ID does not exist.',
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                message: 'The user could not be removed.'
+            });
+        });
+});
+
+
+server.put('/api/users/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res.status(400).json({
+            success: false,
+            message: 'Please provide name and bio for the user',
+        });
+    }
+
+    db
+    .update(id, {name, bio})
+        .then(response => {
+           if (response === 0) {
+               res.status(404).json({
+                   success: false,
+                   message: 'The user with the specified ID does not exist.'
+               });
+           }
+        db
+        .findById(id)
+            .then(user => {
+                if (user.length === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: 'User with that id not found',
+                    })
+                }
+                else {
+                res.status(200).json({
+                    success: true,
+                    message: 'User updated',
+                    user,
+                })
+            }
+            })
+            .catch(error => {
+                res.status(500).json({
+                   success: false,
+                   message: 'The user information could not be modified.' 
+                })
+            })
+        })
+        .catch(error => {
+            res.status(500).json({
+                success: false,
+                message: 'Oh no.'
+            })
         })
 })
+
 
 
 server.listen(5000, () => console.log('Server running on port 5000'));
